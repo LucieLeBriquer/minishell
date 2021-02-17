420d1ba0c5b7eb3af6f5f3fb3733443ae2e342f4
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-02-17 18:06:16 +0100

    cd seems ok

--- sources/builtin/cd.c
+++ sources/builtin/cd.c
@@ -1,10 +1,38 @@
 #include "minishell.h"
 
+char	*get_home(char **env)
+{
+	int		i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "HOME=", 5) == 0)
+			return (ft_split(env[i], "=")[1]);
+		i++;
+	}
+	return ("/");
+}
+
 int	ft_cd(t_info *cmd, t_split *split, char **env)
 {
-	(void)cmd;
-	(void)split;
-	(void)env;
-	ft_printf("\t> cd\n");
-	return (0);
+	char	**args;
+	int		fd;
+
+	args = create_tab_args(cmd, split);
+	if (cmd->number > 2)
+	{
+		ft_printf("minishell: '%s': %s\n", "cd", "too many arguments");
+		return (-1);
+	}
+	if (args[1])
+	{
+		fd = open(args[1], O_RDONLY);
+		if (fd < 0)
+			return (-2);
+		close(fd);
+		return (chdir(ft_strjoin("./", args[1])));
+	}
+	else
+		return (chdir(get_home(env)));
 }
