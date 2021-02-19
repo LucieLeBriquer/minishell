9d3d9498231350c09c196706f544e0a56ea7078b
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-02-19 16:55:17 +0100

    unset ok

--- sources/builtin/unset.c
+++ sources/builtin/unset.c
@@ -2,9 +2,15 @@
 
 int	ft_unset(t_info *cmd, t_split *split, t_list *envl)
 {
-	(void)cmd;
-	(void)split;
-	(void)envl;
-	ft_printf("\t> unset\n");
+	while (envl)
+	{
+		if (is_in_env(envl, split[cmd->start + 1].str))
+		{
+			((t_env *)envl->content)->exported = 0;
+			((t_env *)envl->content)->var = ft_strjoin(split[cmd->start].str, "=''");
+			return (0);
+		}
+		envl = envl->next;
+	}
 	return (0);
 }
