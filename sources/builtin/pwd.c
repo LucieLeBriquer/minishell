027ae45a6b08ff366f8b8cce6fc6c2786d63aeab
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-02-19 10:40:19 +0100

    pwd ok

--- sources/builtin/pwd.c
+++ sources/builtin/pwd.c
@@ -2,9 +2,12 @@
 
 int	ft_pwd(t_info *cmd, t_split *split, char **env)
 {
+	char	cwd[SIZE_PATH];
+	
 	(void)cmd;
 	(void)split;
 	(void)env;
-	ft_printf("\t> pwd\n");
+	getcwd(cwd, SIZE_PATH);
+	ft_printf("%s\n", cwd);
 	return (0);
 }
