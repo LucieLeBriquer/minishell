0aa6cc1ec996e6a390e400e7b7abb469277233b7
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-02-19 17:27:39 +0100

    multiple arguments unset export

--- sources/builtin/export.c
+++ sources/builtin/export.c
@@ -1,30 +1,42 @@
 #include "minishell.h"
 
-int	ft_export(t_info *cmd, t_split *split, t_list *envl)
+void	export_one(char *to_export, t_list *envl)
 {
 	char	*empty;
 	t_list	*save;
 	t_list	*new;
-
-	(void)cmd;
-	(void)split;
-	(void)envl;
+	
 	save = envl; 
-	// si pas d'arguments print dans l'ordre alphabétique
-	if (cmd->number <= 1)
-		return (0);
 	while (envl)
 	{
 		save = envl;
-		if (is_in_env(envl, split[cmd->start + 1].str))
+		if (is_in_env(envl, to_export))
 		{
 			((t_env *)envl->content)->exported = 1;
-			return (0);
+			return ;
 		}
 		envl = envl->next;
 	}
-	empty = ft_strjoin(split[cmd->start + 1].str, "=''");
+	empty = ft_strjoin(to_export, "=''");
 	new = init_entry(empty, 1);
 	save->next = new;
+}
+
+int	ft_export(t_info *cmd, t_split *split, t_list *envl)
+{
+	int		i;
+	char	**args;
+
+	// si pas d'arguments print dans l'ordre alphabétique
+	if (cmd->number <= 1)
+		return (0);
+	args = create_tab_args(cmd, split);
+	i = 1;
+	while (args[i])
+	{
+		export_one(args[i], envl);
+		i++;
+	}
+	free(args);
 	return (0);
 }
--- sources/builtin/unset.c
+++ sources/builtin/unset.c
@@ -1,16 +1,33 @@
 #include "minishell.h"
 
-int	ft_unset(t_info *cmd, t_split *split, t_list *envl)
+void	unset_one(char *to_unset, t_list *envl)
 {
 	while (envl)
 	{
-		if (is_in_env(envl, split[cmd->start + 1].str))
+		if (is_in_env(envl, to_unset))
 		{
 			((t_env *)envl->content)->exported = 0;
-			((t_env *)envl->content)->var = ft_strjoin(split[cmd->start].str, "=''");
-			return (0);
+			((t_env *)envl->content)->var = ft_strjoin(to_unset, "=''");
+			return ;
 		}
 		envl = envl->next;
 	}
+}
+
+int	ft_unset(t_info *cmd, t_split *split, t_list *envl)
+{
+	int		i;
+	char	**args;
+
+	if (cmd->number <= 1)
+		return (0);
+	args = create_tab_args(cmd, split);
+	i = 1;
+	while (args[i])
+	{
+		unset_one(args[i], envl);
+		i++;
+	}
+	free(args);
 	return (0);
 }
