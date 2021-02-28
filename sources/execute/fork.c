9f74b4df756248b318368e299ea35e02a92a8bdb
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-02-28 22:36:12 +0100

    start all pipes at the same time

--- sources/execute/fork.c
+++ sources/execute/fork.c
@@ -15,12 +15,10 @@ int	fork_and_exec(t_info *cmd, t_split *split, t_list *envl, char *file)
 		change_stdin_stdout(cmd);
 		close_unused_fd(cmd);
 		cmd->args = create_tab_args(cmd, split);
-		ft_printf("hello\n");
 		if (file)
 			ret = execve(file, cmd->args, cmd->env);
 		else
 			ret = execve(split[cmd->start].str, cmd->args, cmd->env);
-		ft_printf("%d\n", ret);
 	}
 	return (0);
 }
