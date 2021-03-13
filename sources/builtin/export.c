241caa90bbb12515f370da9ae9a689545019d0c7
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-03-13 18:00:10 +0100

    declaration only when not in subprocess

--- sources/builtin/export.c
+++ sources/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 19:06:38 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/03/13 17:40:11 by lle-briq         ###   ########.fr       */
+/*   Updated: 2021/03/13 17:59:45 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_export(t_info *cmd, t_list **envl)
 {
 	if (number_of_args(cmd->argv + cmd->offset) <= 1)
 		return (print_sorted(*envl, cmd));
-	if (cmd->is_subprocess)
+	if (!cmd->is_subprocess)
 		return (export_all(cmd->argv + cmd->offset + 1, envl, 2));
 	else
 		return (SUCCESS);
